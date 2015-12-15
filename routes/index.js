f72d399f4aae44a7bd0db901727ea76b7884d29c
lijian17 <374452668@qq.com>
2015-12-15 15:29:57 +0800

    首页视图层优化

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
 exports.article = require('./article');
 exports.user = require('./user');
 
-/*
- * GET home page.
+/**
+ * GET 首页
  */
-
 exports.index = function(req, res, next){
   req.collections.articles.find({published: true}, {sort: {_id:-1}}).toArray(function(error, articles){
     if (error) return next(error);
+	//函数res.render(viewName, data, callback(error, html))参数说明如下：（模板，数据，回调）
     res.render('index', { articles: articles});
   })
 };
